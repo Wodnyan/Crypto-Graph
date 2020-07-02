dfca2781e75172a09e795c27d76140c288e367f4
Albert Gergo <gergoalbert.gta@gmail.com>
2020-07-02 11:06:46 +0300

    Extracted the Chart into a component

---
+++ src/components/Chart/Chart.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Tooltip, LineChart, XAxis, YAxis, Line } from "recharts";
+
+// Bitcoin 		XBT/BTC
+// Ether		ETH
+// Litecoin		LTC
+// Ripple XRP	XRP
+
+interface CryptoData {
+    data: object[] | [];
+}
+
+export default function CryptoChart({ data }: CryptoData) {
+    return (
+        <div className="chart-container">
+            <h1>Bitcoin</h1>
+            <LineChart
+                width={700}
+                height={200}
+                data={data}
+                margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+            >
+                {/* <CartesianGrid stroke="#f5f5f5" /> */}
+                <YAxis />
+                <XAxis dataKey="time" />
+                <Tooltip />
+                <Line
+                    type="monotone"
+                    dataKey="close"
+                    stroke="#ff7300"
+                    dot={false}
+                    strokeWidth={5}
+                />
+            </LineChart>
+        </div>
+    );
+}
