ce08e4458b7d28bc6a174c607ade160669cce27e
Albert Gergo <gergoalbert.gta@gmail.com>
2020-07-02 11:07:23 +0300

    Found a better API, I can now query for other cryptocurrencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,45 @@
 import React, { useState, useEffect } from "react";
-import {
-	Tooltip,
-	LineChart,
-	XAxis,
-	YAxis,
-	CartesianGrid,
-	Line,
-} from "recharts";
+import CryptoChart from "./components/Chart/Chart";
 
 function App() {
-	const BITCOIN_API = "https://api.coindesk.com/v1/bpi/historical/close.json";
-	const [data, setData] = useState<object[] | []>([]);
-	useEffect(() => {
-		fetch(BITCOIN_API)
-			.then((resp) => resp.json())
-			.then(({ bpi }) => {
-				console.log(bpi);
-				const myData = Object.keys(bpi).map((date) => {
-					return {
-						price: bpi[date],
-						date: date,
-					};
-				});
-				setData(myData);
-			});
-	}, []);
-	return (
-		<div className="App">
-			<h1>Bitcoin</h1>
-			<LineChart
-				width={400}
-				height={200}
-				data={data}
-				margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
-			>
-				{/* <CartesianGrid stroke="#f5f5f5" /> */}
-				<YAxis />
-				<XAxis dataKey="date" />
-				<Tooltip />
-				<Line
-					type="monotone"
-					dataKey="price"
-					stroke="#ff7300"
-					dot={false}
-					strokeWidth={5}
-				/>
-			</LineChart>
-		</div>
-	);
+    const [cryptoData, setCryptoData] = useState<object[] | []>([]);
+    const [cryptoSymbol, setCryptoSymbol] = useState<string>("BTC");
+    const [currency, setCurrency] = useState<string>("USD")
+    // const BITCOIN_API = "https://api.coindesk.com/v1/bpi/historical/close.json";
+    const CRYPTO_API = `https://min-api.cryptocompare.com/data/v2/histoday?fsym=${cryptoSymbol}&tsym=${currency}&limit=100`;
+    useEffect(() => {
+        fetch(CRYPTO_API)
+            .then((resp) => resp.json())
+			.then(({ Data }) => {
+				const historicalData = Data.Data;
+				console.log(historicalData);
+				setCryptoData(historicalData);
+                // console.log(bpi);
+                // const myData = Object.keys(bpi).map((date) => {
+                //     return {
+                //         price: bpi[date],
+                //         date: date,
+                //     };
+                // });
+                // setData(myData);
+            });
+	}, [cryptoSymbol, setCryptoSymbol]);
+	function handleSelectChange(e: React.ChangeEvent<HTMLSelectElement>) {
+		setCurrency(e.target.value)
+	}
+    return (
+        <div className="App">
+        	<form>
+        		<input />
+        		<select value={currency} onChange={handleSelectChange}>
+        			<option value="EUR">Euro</option>
+        			<option value="USD">Dollar(USD)</option>
+        			<option value="JPY">Yen</option>
+        		</select>
+        	</form>
+            <CryptoChart data={cryptoData} />
+        </div>
+    );
 }
 
 export default App;
