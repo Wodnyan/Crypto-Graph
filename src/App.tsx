ed32da0bfeba2f0e1bcbddb12e18721878b5f388
Albert Gergo <gergoalbert.gta@gmail.com>
2020-07-06 22:24:12 +0300

    Date is parsed from unix timestamp to normal date

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
+//Style
+import "./App.scss";
+//Inteface
 import { currency } from "./interfaces";
+//Data
+import cryptoList from "./data/crypto.json";
+import currencyList from "./data/currency.json";
+//Components
 import CustomSelect from "./components/CustomSelect/CustomSelect";
 import CryptoChart from "./components/Chart/Chart";
 
@@ -10,68 +17,47 @@ function App() {
         code: "BTC",
     });
     const [currency, setCurrency] = useState<currency>({
-        name: "Dollar",
+        name: "Us Dollar",
         code: "USD",
     });
-    const symbolList = [
-        {
-            index: 0,
-            name: "Bitcoin",
-            code: "BTC",
-        },
-        {
-            index: 1,
-            name: "Etherium",
-            code: "ETH",
-        },
-        {
-            index: 2,
-            name: "404",
-            code: "404",
-        },
-    ];
-    const currencyList = [
-        {
-            index: 0,
-            name: "Yen",
-            code: "JPY",
-        },
-        {
-            index: 1,
-            name: "Dollar",
-            code: "USD",
-        },
-        {
-            index: 2,
-            name: "Euro",
-            code: "EUR",
-        },
-    ];
     const CRYPTO_API = `https://min-api.cryptocompare.com/data/v2/histoday?fsym=${cryptoSymbol.code}&tsym=${currency.code}&limit=100`;
     useEffect(() => {
         fetch(CRYPTO_API)
             .then((resp) => resp.json())
             .then(({ Data }) => {
                 console.log(Data.Data);
-                setCryptoData(Data.Data);
+                const formatedData = Data.Data.map((crypto: any) => {
+                    const unixToNormalDate = new Date(crypto.time * 1000);
+                    const date = `${unixToNormalDate.getFullYear()}.${pushZeroToDateNumber(
+                        unixToNormalDate.getMonth() + 1
+                    )}.${pushZeroToDateNumber(unixToNormalDate.getDate())}`;
+                    return {
+                        close: crypto.close,
+                        time: date,
+                    };
+                });
+                setCryptoData(formatedData);
             });
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [cryptoSymbol, currency]);
+    }, [CRYPTO_API]);
     return (
         <div className="App">
-            <CustomSelect
-                selected={cryptoSymbol}
-                setSelected={setCryptoSymbol}
-                optionsList={symbolList}
-            />
-            <CustomSelect
-                selected={currency}
-                setSelected={setCurrency}
-                optionsList={currencyList}
-            />
-            <CryptoChart data={cryptoData} />
+            <section className="search">
+                <CustomSelect
+                    selected={cryptoSymbol}
+                    setSelected={setCryptoSymbol}
+                    optionsList={cryptoList.cryptos}
+                />
+                <CustomSelect
+                    selected={currency}
+                    setSelected={setCurrency}
+                    optionsList={currencyList.currencies}
+                />
+            </section>
+            <CryptoChart data={cryptoData} title={cryptoSymbol.name} />
         </div>
     );
 }
-
+function pushZeroToDateNumber(number: number) {
+    return number < 10 ? "0" + number.toString() : number;
+}
 export default App;
