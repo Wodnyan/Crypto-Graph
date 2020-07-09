1c13d3751b7b8353112c7eb8afc5340417c2cc07
Albert Gergo <gergoalbert.gta@gmail.com>
2020-07-09 22:04:31 +0300

    Cleaned up code and started doing the compare feature

--- src/App.tsx
+++ src/App.tsx
@@ -2,47 +2,47 @@ import React, { useState, useEffect } from "react";
 //Style
 import "./App.scss";
 //Inteface
-import { currency } from "./interfaces";
+import { Currency, CryptoChartData } from "./interfaces";
 //Data
 import cryptoList from "./data/crypto.json";
 import currencyList from "./data/currency.json";
+//Functions
+import { unixToNormalDate, toEuropeanYearFormat } from "./_functions";
 //Components
 import CustomSelect from "./components/CustomSelect/CustomSelect";
 import CryptoChart from "./components/Chart/Chart";
 import CryptoInfo from "./components/CryptoInfo/CryptoInfo";
 
 function App() {
-    const [cryptoData, setCryptoData] = useState<object[] | []>([]);
-    const [cryptoSymbol, setCryptoSymbol] = useState<currency>({
+    const [cryptoData, setCryptoData] = useState<CryptoChartData[] | []>([]);
+    const [cryptoSymbol, setCryptoSymbol] = useState<Currency>({
         name: "Bitcoin",
         code: "BTC",
     });
-    const [currency, setCurrency] = useState<currency>({
+    const [currency, setCurrency] = useState<Currency>({
         name: "Us Dollar",
         code: "USD",
     });
+    const [compare, setCompare] = useState<boolean>(false);
     const CRYPTO_API = `https://min-api.cryptocompare.com/data/v2/histoday?fsym=${cryptoSymbol.code}&tsym=${currency.code}&limit=100`;
+
     useEffect(() => {
-        fetch(CRYPTO_API)
-            .then((resp) => resp.json())
-            .then(({ Data }) => {
-                console.log(Data.Data);
-                const formatedData = Data.Data.map((crypto: any) => {
-                    const unixToNormalDate = new Date(crypto.time * 1000);
-                    const date = `${unixToNormalDate.getFullYear()}.
-                                  ${pushZeroToDateNumber(
-                                      unixToNormalDate.getMonth() + 1
-                                  )}.
-                                  ${pushZeroToDateNumber(
-                                      unixToNormalDate.getDate()
-                                  )}`;
-                    return {
-                        close: crypto.close,
-                        time: date,
-                    };
-                });
-                setCryptoData(formatedData);
+        const fetchData = async () => {
+            const resp = await fetch(CRYPTO_API);
+            const data = await resp.json();
+            const { Data } = data.Data;
+            const formatedData = Data.map((crypto: any) => {
+                const date = toEuropeanYearFormat(
+                    unixToNormalDate(crypto.time)
+                );
+                return {
+                    date,
+                    price: crypto.close,
+                };
             });
+            setCryptoData(formatedData);
+        };
+        fetchData();
     }, [CRYPTO_API]);
     return (
         <div className="App">
@@ -57,7 +57,27 @@ function App() {
                     setSelected={setCurrency}
                     optionsList={currencyList.currencies}
                 />
+                {compare && (
+                    <>
+                        <CustomSelect
+                            selected={cryptoSymbol}
+                            setSelected={setCryptoSymbol}
+                            optionsList={cryptoList.cryptos}
+                        />
+                        <CustomSelect
+                            selected={currency}
+                            setSelected={setCurrency}
+                            optionsList={currencyList.currencies}
+                        />
+                    </>
+                )}
             </section>
+            <button
+                className="compare-button"
+                onClick={() => setCompare((prev) => !prev)}
+            >
+                Compare
+            </button>
             <section className="crypto">
                 <CryptoChart data={cryptoData} title={cryptoSymbol.name} />
                 <CryptoInfo
@@ -67,8 +87,5 @@ function App() {
             </section>
         </div>
     );
-}
-function pushZeroToDateNumber(number: number) {
-    return number < 10 ? "0" + number.toString() : number;
 }
 export default App;
